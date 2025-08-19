fc21431a7525cb56c53f920c4b1eee0460987b14
Ola Johannes Elvedahl <olaelve5@outlook.com>
2025-08-19 18:40:13 +0200

    feat: add page transition effect to WinningPage with timer for showPage state

--- app/src/components/host/game/WinningPage/WinningPage.tsx
+++ app/src/components/host/game/WinningPage/WinningPage.tsx
@@ -3,15 +3,26 @@ import styles from "./styles/WinningPage.module.css";
 import PlayersAccordion from "./PlayersAccoridion";
 import Podium from "./Podium";
 import NewGameButton from "./NavigationButtons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import AwardsSection from "./AwardsSection";
 
 export default function WinningPage() {
   const { game } = useConnectionStore();
   const [podiumAnimationFinished, setPodiumAnimationFinished] = useState(false);
+  const [showPage, setShowPage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPage(true);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   if (!game) return null;
+
+  if (!showPage) return null;
 
   return (
     <div className={styles.container}>
